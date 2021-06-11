5ba3f8ee3917bd0945214540cbb3ec8cc38bcedd
Alpha-Craft-The-Alpha <knolld00@gmail.com>
2021-06-11 15:58:40 +0200

    Change some features and make field bigger

--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from time import time
 from random import randint
+from typing import Union
 
 pygame.init()
 
@@ -65,11 +66,9 @@ hs_text_rect.y -= 5
 
 display_grid = pygame.Surface((500, 500))
 display_grid.set_colorkey((0, 0, 0))
-for i in range(10):
-    for n in range(10):
-        pygame.draw.rect(display_grid, (0, 175, 255), (n % 2 * 50 + i * 50 + i * 50, n * 50, 50, 50))
-
-grid = [[0 for n in range(10)] for i in range(10)]
+for i in range(25):
+    for n in range(25):
+        pygame.draw.rect(display_grid, (0, 175, 255), (n % 2 * 20 + i * 20 + i * 20, n * 20, 20, 20))
 
 game_lost = False
 started = False
@@ -103,7 +102,7 @@ class Snake:
         self.DOWN = 0, 1
         self.directions = self.RIGHT, self.DOWN, self.LEFT, self.UP
         self.direction = 0
-        self.head = [4, 4]
+        self.head = [11, 11]
         self.cursor = self.head.copy()
         self.self_collide_check_cursor = self.head.copy()
         self.body = []
@@ -112,9 +111,10 @@ class Snake:
         self.length = 0
         self.key_pattern = []
         self.wait_for_appending = False
+        self.time_since_click_start = None
 
     def reset(self):
-        self.head = [4, 4]
+        self.head = [11, 11]
         self.length = 0
         self.saved_time = time()
         self.direction = 0
@@ -136,8 +136,8 @@ class Snake:
                     self.direction = self.key_pattern[0]
                     self.key_pattern.pop(0)
 
-                if not screen_rect.contains(Rect((self.head[0] + self.directions[self.direction][0]) * 50,
-                                                 (self.head[1] + self.directions[self.direction][1]) * 50, 50, 50)):
+                if not screen_rect.contains(Rect((self.head[0] + self.directions[self.direction][0]) * 20,
+                                                 (self.head[1] + self.directions[self.direction][1]) * 20, 20, 20)):
                     started = False
                     game_lost = True
                     fade_direction = 0
@@ -160,7 +160,7 @@ class Snake:
 
                 if self.length < 2:
                     if len(self.body) > 0:
-                        self.body.append((4 - (self.head[0] + self.body[-1][0]), 4 - (self.head[1] + self.body[-1][1])))
+                        self.body.append((11 - (self.head[0] + self.body[-1][0]), 11 - (self.head[1] + self.body[-1][1])))
                     else:
                         self.body.append(
                             (self.directions[self.direction][0] * -1, self.directions[self.direction][1] * -1))
@@ -169,41 +169,66 @@ class Snake:
                 self.saved_time = time()
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 219, 5), (self.head[0] * 50, self.head[1] * 50, 50, 50))
+        pygame.draw.rect(surface, (255, 219, 5), (self.head[0] * 20, self.head[1] * 20, 20, 20))
         self.cursor = self.head.copy()
         for v in self.body:
             self.cursor[0] += v[0]
             self.cursor[1] += v[1]
-            pygame.draw.rect(surface, (255, 219, 5), (self.cursor[0] * 50, self.cursor[1] * 50, 50, 50))
+            pygame.draw.rect(surface, (255, 219, 5), (self.cursor[0] * 20, self.cursor[1] * 20, 20, 20))
 
-    def handle_input(self, event: pygame.event.Event):
-        if event.type == KEYDOWN:
-            if len(self.key_pattern) > 0:
-                if event.key == K_RIGHT:
-                    if self.key_pattern[-1] != 2:
-                        self.key_pattern.append(0)
-                if event.key == K_LEFT:
-                    if self.key_pattern[-1] != 0:
-                        self.key_pattern.append(2)
-                if event.key == K_UP:
-                    if self.key_pattern[-1] != 1:
-                        self.key_pattern.append(3)
-                if event.key == K_DOWN:
-                    if self.key_pattern[-1] != 3:
-                        self.key_pattern.append(1)
-            else:
-                if event.key == K_RIGHT:
-                    if self.direction != 2:
-                        self.key_pattern.append(0)
-                if event.key == K_LEFT:
-                    if self.direction != 0:
-                        self.key_pattern.append(2)
-                if event.key == K_UP:
-                    if self.direction != 1:
-                        self.key_pattern.append(3)
-                if event.key == K_DOWN:
-                    if self.direction != 3:
-                        self.key_pattern.append(1)
+    def handle_input(self, event: Union[pygame.event.Event] = None):
+        if event:
+            if event.type == KEYDOWN:
+                if len(self.key_pattern) > 0:
+                    if event.key == K_RIGHT:
+                        if self.key_pattern[-1] != 2:
+                            self.key_pattern.append(0)
+                    if event.key == K_LEFT:
+                        if self.key_pattern[-1] != 0:
+                            self.key_pattern.append(2)
+                    if event.key == K_UP:
+                        if self.key_pattern[-1] != 1:
+                            self.key_pattern.append(3)
+                    if event.key == K_DOWN:
+                        if self.key_pattern[-1] != 3:
+                            self.key_pattern.append(1)
+                else:
+                    if event.key == K_RIGHT:
+                        if self.direction != 2:
+                            self.key_pattern.append(0)
+                    if event.key == K_LEFT:
+                        if self.direction != 0:
+                            self.key_pattern.append(2)
+                    if event.key == K_UP:
+                        if self.direction != 1:
+                            self.key_pattern.append(3)
+                    if event.key == K_DOWN:
+                        if self.direction != 3:
+                            self.key_pattern.append(1)
+
+        keys = pygame.key.get_pressed()
+        if keys[K_RIGHT] and self.direction == 0:
+            if self.time_since_click_start is None:
+                self.time_since_click_start = time()
+            if time() - self.time_since_click_start >= .5:
+                self.tbs *= .5
+        elif keys[K_DOWN] and self.direction == 1:
+            if self.time_since_click_start is None:
+                self.time_since_click_start = time()
+            if time() - self.time_since_click_start >= .5:
+                self.tbs *= .5
+        elif keys[K_LEFT] and self.direction == 2:
+            if self.time_since_click_start is None:
+                self.time_since_click_start = time()
+            if time() - self.time_since_click_start >= .5:
+                self.tbs *= .5
+        elif keys[K_UP] and self.direction == 3:
+            if self.time_since_click_start is None:
+                self.time_since_click_start = time()
+            if time() - self.time_since_click_start >= .5:
+                self.tbs *= .5
+        elif self.time_since_click_start:
+            self.time_since_click_start = None
 
     def food_collision(self):
         if tuple(self.head) == food.grid_pos:
@@ -250,7 +275,7 @@ class Food:
 
     def randomize_position(self):
         while True:
-            self.grid_pos = (randint(0, 9), randint(0, 9))
+            self.grid_pos = (randint(0, 24), randint(0, 24))
             if len(snake.key_pattern) == 0:
                 if snake.snake_collides_with(self.grid_pos) or snake.snake_collides_with((self.grid_pos[0] - snake.directions[snake.direction][0], self.grid_pos[1] - snake.directions[snake.direction][1])):
                     continue
@@ -263,7 +288,7 @@ class Food:
                     break
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (254, 1, 0), (self.grid_pos[0] * 50 + 10, self.grid_pos[1] * 50 + 10, 30, 30))
+        pygame.draw.rect(surface, (254, 1, 0), (self.grid_pos[0] * 20 + 4, self.grid_pos[1] * 20 + 4, 12, 12))
 
 
 def change_text_transparency():
@@ -307,6 +332,7 @@ while run:
                     food.randomize_position()
         if started:
             snake.handle_input(event)
+    snake.handle_input()
 
     screen.fill((0, 200, 255))
     screen.blit(display_grid, (0, 0))
@@ -329,9 +355,9 @@ while run:
             snake.draw(screen)
             draw_score()
             if snake.length > 1:
-                snake.tbs = .25 - (snake.length - 2) * .005
-                if snake.tbs < .125:
-                    snake.tbs = .125
+                snake.tbs = .25 - (snake.length - 2) * .01
+                if snake.tbs < .1:
+                    snake.tbs = .1
 
     pygame.display.update()
     clock.tick(60)
